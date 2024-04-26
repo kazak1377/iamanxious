c5ee427972f9974c349ef663d3c08eea7ca4c08e
Maksym Kozachenko <max@kozachenko.io>
2024-04-26 01:17:00 +0000

    Some basic styles

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="bg-slate-700">
+    <div
+        class="flex items-center justify-center content-center bg-gradient-to-tr from-[#100c1a] to-[#150c1a]"
+    >
         <slot></slot>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,49 @@
 definePageMeta({
     layout: "landing",
 });
-
-const { $client } = useNuxtApp();
-const resp = await $client.hello.query({ text: "Max" });
-const worries = await $client.worries.latest.query();
 </script>
 
 <template>
     <div>
-        <h1>Home</h1>
-        <p>{{ resp.greeting }}</p>
-        <ul>
-            <li v-for="worry in worries.records">{{ worry.worry_text }}</li>
-        </ul>
+        <section class="max-w-screen-md mx-auto mt-40 mb-24">
+            <h1
+                class="text-7xl p-4 text-left font-bold from-purple-300 via-pink-300 to-blue-300 bg-gradient-to-r bg-clip-text text-transparent"
+            >
+                I am Anxious – <br />
+                You're Not Alone
+            </h1>
+            <p class="text-3xl p-4 font-semibold text-gray-300">
+                A private haven where you can unburden your anxieties
+                anonymously and safely.
+            </p>
+        </section>
+        <section class="max-w-screen-md mx-auto mt-40 mb-24">
+            <h2 class="text-4xl p-4 font-bold text-gray-300">How it Works</h2>
+            <p class="text-xl p-4 text-gray-100">
+                Everyone has moments of doubt and anxiety, but no one should
+                have to face them alone. 'I am Anxious' provides a space where
+                you can share your fears and worries without revealing your
+                identity. Here, you can express yourself freely, browse the
+                concerns of others, and find solace in our shared human
+                experiences. Let’s navigate the complexities of life together,
+                anonymously.
+            </p>
+            <div
+                class="flex gap-4 flex-row items-center justify-start mt-10 p-4"
+            >
+                <nuxt-link
+                    to="/share"
+                    class="rounded-xl bg-purple-800 text-white px-10 py-4"
+                >
+                    Share your worries
+                </nuxt-link>
+                <nuxt-link
+                    to="/read"
+                    class="rounded-xl bg-lime-700 text-white px-10 py-4"
+                >
+                    Read others' worries
+                </nuxt-link>
+            </div>
+        </section>
     </div>
 </template>
