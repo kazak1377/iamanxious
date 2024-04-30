8d5335d19e411c76b568ec796cc8ecc3183c1d8d
Max Kozachenko <max@kozachenko.io>
2024-04-30 17:52:56 +0000

    updated about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,36 @@
 <template>
-    <section class="pt-20 max-w-screen-md mx-auto">
+    <section class="pt-28 max-w-screen-md mx-auto">
         <glass-card>
             <h2 class="text-4xl p-4 font-bold text-gray-300">
                 About "I Am Anxious" project
             </h2>
             <p class="text-xl p-4 text-gray-100">
-                I Am Anxious is a project that aims to help people with anxiety
-                to share their worries and read worries of others. It is a
-                non-profit project that is developed by Max Kozachenko.
+                <strong>I Am Anxious</strong> is a non-profit initiative
+                designed by Alla Medvedieva and developed by Max Kozachenko to
+                support individuals coping with anxiety. This platform provides
+                a secure space for users to anonymously share their concerns and
+                connect with others experiencing similar challenges.
             </p>
             <p class="text-xl p-4 text-gray-100">
-                This project is completely anonymous and does not require any
-                personal information to share or read worries. No cookies and
-                any other tracking technologies are used.
+                <strong>Our commitment to privacy is paramount:</strong> <br />
             </p>
+            <ul class="text-xl text-gray-100 list-disc pl-10">
+                <li>
+                    Complete Anonymity: No personal information is required to
+                    participate.
+                </li>
+                <li>
+                    No Tracking: We do not use cookies or any tracking
+                    technologies.
+                </li>
+            </ul>
+            <p></p>
             <p class="text-xl p-4 text-gray-100">
-                If you have any questions or suggestions, feel free to contact
-                me at
+                We strive to maintain and improve this platform as long as
+                possible. While the future is uncertain, our dedication to this
+                cause remains unwavering. If you have questions, suggestions, or
+                wish to support our hosting efforts, please reach out to us at
+
                 <a
                     href="mailto:iamanxious@kozachenko.io"
                     class="text-fuchsia-300"
@@ -25,10 +39,57 @@
                 </a>
             </p>
             <p class="text-xl p-4 text-gray-100">
-                I cannot guarantee that this project will exist forewer, but I
-                am doing my best to keep it alive. If you want to help me with
-                hosting costs, contact me via email above. Current state of
-                monitization
+                <strong>Monetization Policy:</strong> Our financial strategies
+                are still being defined. However, we pledge never to sell any
+                user information.
+            </p>
+        </glass-card>
+        <glass-card class="mt-10">
+            <h2 class="text-4xl p-4 font-bold text-gray-300">
+                Meet the Creators
+            </h2>
+            <p class="text-xl p-4 text-gray-100">
+                Hello! We are Alla Medvedieva and Max Kozachenko, the minds
+                behind the "I Am Anxious" project. Alla excels in creating
+                intuitive and engaging user experiences, while Max ensures
+                robust software functionality and technical expertise.
+            </p>
+            <p class="text-xl p-4 text-gray-100">
+                <strong>Flexible Collaboration for Your Needs:</strong> <br />
+                Looking for comprehensive design and development or just one of
+                these services? Whether you need Alla’s design prowess, Max’s
+                development expertise, or both, we are here to adapt to the
+                specific requirements of your project.
+            </p>
+            <p class="text-xl p-4 text-gray-100">
+                <strong>Our commitment to privacy is paramount:</strong> <br />
+            </p>
+            <ul class="text-xl text-gray-100 list-disc pl-10">
+                <li>
+                    Design and Development Expertise: From concept creation to
+                    the final product.
+                </li>
+                <li>
+                    Tailored Solutions: Custom services that align perfectly
+                    with your project’s goals.
+                </li>
+                <li>
+                    Commitment to Excellence: We bring the same dedication to
+                    quality and privacy to every project as we have to "I Am
+                    Anxious."
+                </li>
+            </ul>
+            <p class="text-xl p-4 text-gray-100">
+                If you're interested in exploring how we can contribute to your
+                project, either together or individually, please don't hesitate
+                to contact us at
+
+                <a
+                    href="mailto:projects@kozachenko.io"
+                    class="text-fuchsia-300"
+                >
+                    projects@kozachenko.io
+                </a>
             </p>
         </glass-card>
     </section>
