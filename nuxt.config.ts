6cb70e0362da0d65b8f271da21711174caf514fe
Max Kozachenko <max@kozachenko.io>
2024-06-17 15:40:34 +0000

    Added gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss"],
+    modules: ["@nuxtjs/tailwindcss", "nuxt-gtag"],
+    gtag: {
+        id: "AW-16553909662",
+    },
     build: {
         transpile: ["trpc-nuxt"],
     },
