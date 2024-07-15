f7e39070aac715472a41bb7bc730e656ef1ae70b
Max Kozachenko <max@kozachenko.io>
2024-07-15 10:49:25 +0000

    Removed gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "nuxt-gtag"],
-    gtag: {
-        id: "AW-16553909662",
-    },
+    modules: ["@nuxtjs/tailwindcss"],
     build: {
         transpile: ["trpc-nuxt"],
     },
